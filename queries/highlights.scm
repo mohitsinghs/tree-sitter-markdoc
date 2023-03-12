7cad9cd8ee6feb7fd02d86a0834530915b8df8a5
Mohit Singh <mohit@mohitsingh.in>
2023-03-12 09:48:53 +0530

    fix(highlights): highlight delimiters correctly

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,9 +1,10 @@
 ; highlights.scm
 
 tag_name: (identifier) @tag
-([(tag_close) (tag_self_closing)] "/" @tag.delimiter)
-([(tag_start) (tag_end)] @tag.delimiter)
-(attribute [key : (identifier) "="] @tag.attribute)
+(tag_self_closing "/" @tag.delimiter)
+(tag_close "/" @tag.delimiter)
+([(tag_start) (tag_end) "="] @tag.delimiter)
+(attribute [key : (identifier)] @tag.attribute)
 (attribute [shorthand : (identifier)]  @property)
 (variable [variable : (identifier) (variable_sigil)] @variable)
 (variable_tail property : (identifier) @property)
